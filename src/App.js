9e94d6ff816acbaf3a4523c12481cb073e78fc4a
Mads Elberts <mads.elberts@gmail.com>
2022-12-05 16:12:16 +0900

    Comment inserted

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ function App() {
 
     <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+      {/* <!-- Insert Comment --!> */}
             <h2 className="mb-8 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 lg:mb-16 dark:text-white md:text-4xl">You’ll be in good company</h2>
             <div className="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
                 <a href="#" className="flex justify-center items-center">
